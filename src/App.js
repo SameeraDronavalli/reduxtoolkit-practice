40ad5e4fb14906519e8e240f3e88a96fb9f1421d
Sameera Dronavalli <sameeradronavalli327@gmail.com>
2026-01-02 14:49:23 +0530

    added comments to code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import './App.css';
 import Header from './components/header';
 import ApiDataComponent from './components/apiDataComponent';
 function App() {
   return (
     <>
-
-
       <Header />
       <ApiDataComponent />
-
     </>
   );
 }
-
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { apiDataSlice } from '../features/products/apiproductsSlice';
+import { apiDataSlice } from '../features/products/apiproductsSlice';// This slice handles API products data, including loading, success, and error states
+
+// The store holds the global state of the application
 
 export const store = configureStore({
   reducer: {
